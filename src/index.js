81ae39e7aab72889f30f8993f6844e8036a649b6
veer322 <veeranjaneya.n@accionlabs.com>
2024-01-11 23:02:14 +0530

    added promise.all custom rule

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const nameLength = require('./name-length.js');
+const setPromise = require('./set-promise.js');
 
 module.exports = {
   rules: {
-    'name-length': nameLength
+    'name-length': nameLength,
+    'set-promise': setPromise
   }
 };
---
+++ src/set-promise.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  meta: {
+    type: "problem",
+        docs: {
+            description: "when even Promise.all is used, await should be added to it.",
+        },
+        fixable: "code",
+  },
+  create: context => {
+    return {
+       callExpression(node) {
+        const checkPromise = 
+        node.callee.type === "MemberExpression" &&
+        node.callee.object.type === "Identifier" &&
+        node.callee.object.name === "Promise" &&
+        node.callee.property.type === "Identifier" &&
+        node.callee.property.name === "Promise" 
+
+        if(checkPromise) {
+            context.report({
+                node,
+                message: "Add 'await' before Promise.all()",
+                fix: function (fixer) {
+                  return fixer.insertTextBefore(node, "await ");
+                },
+        })
+       }
+     }
+   }
+ }
+}
