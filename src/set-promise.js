194c9359e026fba1818a22e2ee38bbf1819f7b2d
veer322 <veeranjaneya.n@accionlabs.com>
2024-01-12 10:39:02 +0530

    added set-promise for prommise.all

--- src/set-promise.js
+++ src/set-promise.js
@@ -3,31 +3,31 @@
 module.exports = {
   meta: {
     type: "problem",
-        docs: {
-            description: "when even Promise.all is used, await should be added to it.",
-        },
-        fixable: "code",
+    docs: {
+      description: "when even Promise.all is used, await should be added to it.",
+    },
+    fixable: "code", 
   },
   create: context => {
     return {
-       callExpression(node) {
-        const checkPromise = 
-        node.callee.type === "MemberExpression" &&
-        node.callee.object.type === "Identifier" &&
-        node.callee.object.name === "Promise" &&
-        node.callee.property.type === "Identifier" &&
-        node.callee.property.name === "Promise" 
+      CallExpression(node) { 
+        const checkPromise =
+          node.callee.type === "MemberExpression" &&
+          node.callee.object.type === "Identifier" &&
+          node.callee.object.name === "Promise" &&
+          node.callee.property.type === "Identifier" &&
+          node.callee.property.name === "all";
 
-        if(checkPromise) {
-            context.report({
-                node,
-                message: "Add 'await' before Promise.all()",
-                fix: function (fixer) {
-                  return fixer.insertTextBefore(node, "await ");
-                },
-        })
-       }
-     }
-   }
- }
-}
+        if (checkPromise) {
+          context.report({
+            node,
+            message: "Add 'await' before Promise.all()",
+            fix: function (fixer) {
+              return fixer.insertTextBefore(node, "await ");
+            },
+          });
+        }
+      },
+    };
+  },
+};
